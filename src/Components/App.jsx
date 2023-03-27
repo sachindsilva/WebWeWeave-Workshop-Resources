fdb55b05881beb57586468336f22e2a01a6517f3
sachindsilva16 <sachindsilvad@gmail.com>
2023-03-27 21:48:18 +0530

    Initial commit

---
+++ src/Components/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+
+function App(){
+    return(
+        <div>
+            <h1 className="block font-mono font-bold text-4xl mx-[500px] text-sky-700 my-2 mt-6 text-center border border-1 rounded-md">
+      " THE WEB WE WEAVE "
+    </h1>
+
+    <h2 className="block text-center font-appleEmoji text-gray-400 font-light text-sm">Hands-on workshop for Reactjs | Tailwindcss</h2>
+
+    <div className="font-appleEmoji border block rounded-md max-auto my-4 mt-20 mx-8 px-6 py-2 h-12 align-middle cursor-pointer text-2xl text-sky-600">
+
+        <h1>Starter Files</h1>
+        <a  href="#" className="">
+           
+        </a>
+    </div>
+    <div className="font-appleEmoji border block rounded-md max-auto my-4 mx-8 px-6 py-2 h-12 align-middle cursor-pointer text-2xl text-sky-600">
+        <a  href="#" className="">
+            Colors
+        </a>
+    </div>
+    <div className="font-appleEmoji border block rounded-md max-auto my-4 mx-8 px-6 py-2 h-12 align-middle cursor-pointer text-2xl text-sky-600">
+        <a  href="#" className="">
+            Colors
+        </a>
+    </div>
+        </div>
+    );
+}
+
+export default App;
