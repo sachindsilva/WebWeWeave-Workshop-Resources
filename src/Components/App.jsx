38a5e3b27e0ddeaf5fcb02fbc04954911052dbb4
sachindsilva16 <sachindsilvad@gmail.com>
2023-03-28 23:46:46 +0530

    Modified some stylings

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -9,9 +9,17 @@ function App() {
             <Heading/>
             <SubHeading />
 
+            <h1 className="block text-center font-appleEmoji font-bold text-3xl text-sky-900 mt-20 select-none">
+           Workshop Resources List
+        </h1>
+            <h1 className="block text-center font-appleEmoji font-semibold text-2xl text-sky-900 mt-20 leading-5 space-x-10 select-none">
+            Session1: Reactjs - Front End Framework
+        </h1>
+
+
             <div className="font-appleEmoji border block rounded-md max-auto my-4 mt-20 mx-8 px-6 py-2 h-12 align-middle cursor-pointer text-2xl text-sky-600">
 
-
+             
             </div>
         </div>
     );
--- src/Components/Heading.jsx
+++ src/Components/Heading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Heading() {
     return (
-        <h1 className="block font-mono font-bold text-4xl  text-sky-700 my-2 mt-6 text-center bg-sky-50 rounded-lg m-4 md:mx-x-100">
+        <h1 className="block font-mono font-bold text-4xl  text-sky-900 my-2 mt-6 text-center bg-sky-50 rounded-lg m-4 md:mx-x-100 select-none">
             "THE WEB WE WEAVE"
         </h1>
     );
