79bb25f0110b39ad5223267487e4030915aa937d
sachindsilva16 <sachindsilvad@gmail.com>
2023-03-29 20:55:58 +0530

    Added some stylings

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,10 +4,16 @@ import React from "react";
 function Footer() {
     return (
         <footer>
-            <div className="relative bottom-0 top-0 flex flex-col justify-center items-center md:justify-start md:flex-row space-y-5 md:space-y-0 px-20 w-full h-40 mt-20 p-10 bg-gray-200 font-medium font-appleEmoji text-gray-400 text-opacity-70 select-none">
+            <div className="relative bottom-0 top-0 flex flex-col justify-center items-center md:justify-start md:flex-row space-y-5 md:space-y-0 md:px-20 md:space-x-10  w-full h-40 mt-20 p-10 bg-gray-200 font-medium font-appleEmoji text-gray-400 text-opacity-70  select-none grid grid-cols-1 md:grid-cols-3">
 
-                <p>&copy; The Web We Weave</p>
-                 <div className="flex items-center justify-center md:px-60 letter-spacing">Made with ♥ by Sachin Dsilva</div>
+                <div className="flex justify-center items-center md:justify-start border-1 px-4 md:px-10 py-5 border border-1 border-red-500">
+                    <p>&copy; The Web We Weave</p>
+                </div>
+
+                <div className="flex items-center border-1 justify-center px-10 py-5 border-gray-900 md:px-8 letter-spacing border border-1 border-red-500">Made with ♥ by Sachin Dsilva</div>
+
+                
+                <div className="flex items-center border-1 justify-center px-10 py-5 border-gray-900 md:px-8 letter-spacing border border-1 border-red-500">Made with ♥ by Sachin Dsilva</div>
             </div>
         </footer>
     );
