473410a7c08b8175a411a1529e67bd0b516f7902
sachindsilva16 <sachindsilvad@gmail.com>
2023-03-29 21:53:48 +0530

    Modified some Animations

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,33 +4,47 @@ import React from "react";
 function Footer() {
     return (
         <footer>
-            <div className="relative bottom-0 top-0 flex flex-col justify-center items-center md:justify-start md:flex-row space-y-5 md:space-y-0 md:px-20 md:space-x-10  w-full h-40 mt-20 p-10 bg-gray-200 font-medium font-appleEmoji text-gray-400 text-opacity-70  select-none grid grid-cols-1 md:grid-cols-3">
+            <div className="relative bottom-0 top-0 flex flex-col justify-center items-center md:justify-start md:flex-row space-y-5 md:space-y-0 md:px-20 md:space-x-10  w-full h-auto mt-20 p-10 bg-gray-200 font-medium font-appleEmoji text-gray-400 text-opacity-70  select-none grid grid-cols-1 md:grid-cols-3">
 
-                <div className="flex justify-center items-center md:justify-start border-1 px-4 md:px-10 py-5 border border-1 border-red-500">
+                <div className="flex justify-center items-center md:justify-start border-1 px-4 md:px-10 py-5 ">
                     <p>&copy; The Web We Weave</p>
                 </div>
 
-                <div className="flex items-center border-1 justify-center px-10 py-5 border-gray-900 md:px-8 letter-spacing border border-1 border-red-500">Made with ♥ by Sachin Dsilva</div>
+                <div className="flex items-center border-1 justify-center px-10 py-5 border-gray-900 md:px-8 letter-spacing ">Made with ♥ by Sachin Dsilva</div>
 
 
-                <div className="flex items-center border-1 justify-center px-10 py-5 border-gray-900 md:px-8 letter-spacing border border-1 border-red-500 space-x-3 hover:grayscale ">
+                <div className="flex items-center border-1 justify-center px-10 py-5 border-gray-900 md:px-8 letter-spacing  space-x-3 ">
 
                     {/* GITHUB  */}
-                    <a href="#" className="hover:scale-110  duration-200">
-                    <i class="fa-brands fa-github fa-beat-fade fa-xl " style={{color: "#080808"}}></i>
+                    <a href="#" className="hover:scale-110  hover:grayscale duration-200">
+                    <i className="fa-brands fa-github fa-beat-fade fa-xl " style={{color: "#080808"}}></i>
                     </a>
 
                     {/* INSTAGRAM  */}
-                    <a href="#" className="hover:scale-110">
-                    <i class="fa-brands fa-instagram fa-beat-fade fa-xl" style={{color: "#f71844"}}></i>
+                    <a href="#" className="hover:scale-110 hover:grayscale duration-200">
+                    <i className="fa-brands fa-instagram fa-beat-fade fa-xl" style={{color: "#f71844"}}></i>
                     </a>
 
 
 
                     {/* LINKEDIN  */}
 
-                    <a href="#" className="hover:scale-110">
-                    <i class="fa-brands fa-instagram fa-beat-fade fa-xl" style={{color: "#f71844"}}></i>
+                    <a href="#" className="hover:scale-110 hover:grayscale duration-200">
+                    <i className="fa-brands fa-linkedin fa-beat-fade fa-xl" style={{color: "#1a6eff"}}></i>
+                    </a>
+
+
+                    {/* TWITTER  */}
+                    
+                    
+                    <a href="#" className="hover:scale-110 hover:grayscale duration-200">
+                    <i className="fa-brands fa-twitter fa-beat-fade fa-xl" style={{color: "#1899fb"}}></i>
+                    </a>
+
+                    {/* TELEGRAM  */}
+                    
+                    <a href="#" className="hover:scale-110 hover:grayscale duration-200">
+                    <i className="fa-brands fa-telegram fa-beat-fade fa-xl" style={{color: "#7370ff"}}></i>
                     </a>
                 </div>
             </div>
