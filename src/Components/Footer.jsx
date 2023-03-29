dc732d99187d6b5cbcc97e8be4bf4346bafba72d
sachindsilva16 <sachindsilvad@gmail.com>
2023-03-29 20:45:16 +0530

    Modified few lines

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,10 +4,10 @@ import React from "react";
 function Footer() {
     return (
         <footer>
-            <div className="relative bottom-0 top-0 flex flex-col justify-center items-center md:justify-start md:flex-row space-y-5 md:space-y-0  w-full h-40 mt-20 p-10 bg-gray-200 font-medium font-appleEmoji text-gray-400 text-opacity-70 select-none">
+            <div className="relative bottom-0 top-0 flex flex-col justify-center items-center md:justify-start md:flex-row space-y-5 md:space-y-0 px-20 w-full h-40 mt-20 p-10 bg-gray-200 font-medium font-appleEmoji text-gray-400 text-opacity-70 select-none">
 
                 <p>&copy; The Web We Weave</p>
-                 <div className="flex items-center justify-center md:ml-60 letter-spacing">Made with ♥ by Sachin Dsilva</div>
+                 <div className="flex items-center justify-center md:px-60 letter-spacing">Made with ♥ by Sachin Dsilva</div>
             </div>
         </footer>
     );
